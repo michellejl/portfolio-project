61bde95107fe822355b848abf288e554ef279b12
Michelle JL <michelle@michellejl.com>
2017-07-27 19:27:52 -0700

    more updates

--- gruntfile.js
+++ gruntfile.js
@@ -7,14 +7,11 @@ module.exports = function(grunt) {
 
   // Grunt Tasks by topic:
   require('./grunt_tasks/sass.js')(grunt, config);
-  require('./grunt_tasks/javascript.js')(grunt, config);
   require('./grunt_tasks/images.js')(grunt, config);
 
   grunt.registerTask('default', [
 		'sass',
     'csslint',
-    'concat',
-    'jshint',
     'responsive_images',
     'copy',
     'watch'
